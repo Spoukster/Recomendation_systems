6a69717ff509fa5dcc2534e163fde8009df44d89
Spoukster <spoukster@gmail.com>
2020-05-27 16:56:35 +0300

    Выполнено ДЗ второго урока.

---
+++ src/metrics.py
@@ -0,0 +1,126 @@
+import numpy as np
+
+def precision(recommended_list, bought_list):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    flags = np.isin(bought_list, recommended_list)
+    
+    precision = flags.sum() / len(recommended_list)
+    
+    return precision
+
+
+def precision_at_k(recommended_list, bought_list, k=5):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    bought_list = bought_list  # Тут нет [:k] !!
+    recommended_list = recommended_list[:k]
+    
+    flags = np.isin(bought_list, recommended_list)
+
+    precision = flags.sum() / len(recommended_list)
+    
+    
+    return precision
+
+
+def money_precision_at_k(recommended_list, bought_list, prices_recommended, k=5):
+        
+    # Лучше считать через скалярное произведение, а не цикл
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list[:k])
+    prices_recommended = np.array(prices_recommended[:k])
+    
+    flags = np.isin(recommended_list, bought_list)
+    
+    precision = np.dot(flags, prices_recommended) / np.sum(prices_recommended)
+    
+    return precision
+
+
+def recall(recommended_list, bought_list):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    flags = np.isin(bought_list, recommended_list)
+    
+    recall = flags.sum() / len(bought_list)
+    
+    return recall
+
+
+def recall_at_k(recommended_list, bought_list, k=5):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list[:k])
+    
+    flags = np.isin(bought_list, recommended_list)
+    
+    recall = flags.sum() / len(bought_list)
+    
+    return recall
+
+
+def money_recall_at_k(recommended_list, bought_list, prices_recommended, prices_bought, k=5):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list[:k])
+    prices_recommended = np.array(prices_recommended[:k])
+    prices_bought = np.array(prices_bought)
+    
+    flags = np.isin(recommended_list, bought_list)
+    
+    recall = np.dot(flags, prices_recommended) / np.sum(prices_bought)
+    
+    return recall
+
+
+def ap_k(recommended_list, bought_list, k=5):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    flags = np.isin(recommended_list, bought_list)
+    
+    if sum(flags) == 0:
+        return 0
+    
+    sum_ = 0
+    for i in range(1, k+1):
+        if flags[i-1] == True:
+            p_k = precision_at_k(recommended_list, bought_list, k=i)
+            sum_ += p_k
+            
+    result = sum_ / sum(flags)
+    
+    return result
+
+
+def map_k(recommended_list, bought_list, k=5, u=5):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    sum_ = 0
+    
+    for i in range(1, u+1):
+        sum_ += ap_k(recommended_list, bought_list, k=5)
+        
+    result = sum_ / u
+    
+    return result
+
+
+def reciprocal_rank(recommended_list, bought_list, k=5):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list[:k])
+    
+    flags = np.isin(recommended_list, bought_list)
+    k = np.where(flags == True)[0][0] + 1
+    
+    return 1 / k
